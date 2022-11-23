831e0cebdf5deda2f69c4a96a9f746b12b2d1c20
desshub <desshub95@gmail.com>
2022-11-23 16:54:03 +0300

    Design tracks cards

--- src/components/atoms/Navbar.tsx
+++ src/components/atoms/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="py-2 text-center mb-5">
+        <nav className="pt-2 pb-3 text-center bg-gray-100">
             <Link to="/" className="text-3xl font-bold text-teal-500 hover:text-teal-300">Music Library</Link>
         </nav>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useState, ChangeEvent } from "react";
 import Layout from "../components/layouts/Layout";
 import {Option} from "../components/molecules/Select";
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { Link } from "react-router-dom";
 
 const Home = () => {
     const options: Option[] = [
@@ -12,6 +13,7 @@ const Home = () => {
     ];
     const [search, setSearch] = useState<string>('');
     const [type, setType] = useState<string>('q');
+    const tracks = [1,2,3,4,5,6,7,8,9]
 
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value)
@@ -24,31 +26,48 @@ const Home = () => {
 
     return (
         <Layout>
-            <div className="w-full flex justify-center p-5 shadow-sm">
-                <div className=" bg-gray-300 inset-y-0 flex items-center border-1 border-slate-600">
-                    <select
-                        id="type"
-                        name="type"
-                        className="h-full bg-gray-100 rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-teal-500 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
-                        onChange={handleSelect}
-                        value={type}
-                    >
-                        {options.map(item =>(<option key={item.name} value={item.name}>{item.title}</option>))}
-                    </select>
+            <>
+                <div className="w-full flex justify-center p-5 shadow-sm bg-gray-100">
+                    <div className=" bg-gray-300 inset-y-0 flex items-center border-1 border-slate-600">
+                        <select
+                            id="type"
+                            name="type"
+                            className="h-full bg-gray-100 rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-teal-500 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
+                            onChange={handleSelect}
+                            value={type}
+                        >
+                            {options.map(item =>(<option key={item.name} value={item.name}>{item.title}</option>))}
+                        </select>
+                    </div>
+                    <input
+                        type="text"
+                        name="serach"
+                        id="search"
+                        value={search}
+                        className="block w-3/4 h-10 pl-1 bg-gray-100 border-2 border-gray-200 border-r-0 focus:outline-none sm:text-sm"
+                        placeholder="Eminem"
+                        onChange={onChange}
+                    />
+                    <div className="bg-gray-100 border-2 border-gray-200 border-l-0 rounded-r-sm inset-y-0 flex items-center px-2">
+                    <MagnifyingGlassIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </div>
                 </div>
-                <input
-                    type="text"
-                    name="serach"
-                    id="search"
-                    value={search}
-                    className="block w-3/4 h-10 pl-1 bg-gray-100 border-2 border-gray-200 border-r-0 focus:outline-none sm:text-sm"
-                    placeholder="Eminem"
-                    onChange={onChange}
-                />
-                <div className="bg-gray-100 border-2 border-gray-200 border-l-0 rounded-r-sm inset-y-0 flex items-center px-2">
-                  <MagnifyingGlassIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:gap-4 w-full px-10 py-5">
+                    {tracks.map((track) => (
+                        <div className="bg-gray-100 rounded-lg border-2 border-slate-200 mb-5" key={track}>
+                            <div className="w-full h-auto bg-teal-100">
+                                <img src="https://api.deezer.com/artist/288166/image" alt="Song cover" className="w-full rounded-t-sm" />
+                            </div>
+                            <div className="p-5">
+                                <div className="text-teal-600 font-bold text-xl">Baby</div>
+                                <div className="text-sm font-thin">By <Link to="/" className="text-sm text-sky-700 font-bold underline">Justin Bieber</Link></div>
+                                <div className="text-sm font-light"><span className="text-xs font-semibold">Album:</span> My World 2.0</div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            </div>
+            </>
         </Layout>
     )
 }
